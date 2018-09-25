15cccbecd4ef422b206ea59bd60f0a04847057a1
James Stuckey <jstuckey@rutgers.edu>
2018-09-24 21:46:32 -0400

    added an increment for a simple total

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './styles/App.module.css';
 
-class App extends Component {
-  render() {
-    return <h1>Hello</h1>
-  }
+class App extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            total: 0
+        }
+    }
+
+    fb() {
+        this.setState({
+            total: this.state.total + 1
+        })
+    }
+
+    render() {
+
+        return (
+            <main>
+                <p>{this.state.total}</p>
+                <button className={styles.button} onClick={() => this.fb()}>1</button>
+                <button className={styles.button}>2</button>
+                <button className={styles.button}>3</button>
+            </main>
+        )
+
+    }
 }
 
 export default App;
